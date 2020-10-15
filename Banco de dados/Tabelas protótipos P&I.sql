80d8acb567a12578fe749c8a94b24fec35be151f
MiguelNunesA <miguel.assuncao@bandtec.com.br>
2020-10-15 14:31:34 -0300

    Adicionei tabela de tipo de pessoa

--- Banco de dados/Tabelas protótipos P&I.sql
+++ Banco de dados/Tabelas protótipos P&I.sql
@@ -1,16 +1,24 @@
 create database projeto_pi;
 use projeto_pi;
 
-create table cadastro_usuario(
-	id_usuario int primary key auto_increment,
+create table cadastro_pessoa_fisica(
+	id_pessoa_fisica int primary key auto_increment,
 	nome_completo varchar(100) not null,
 	cpf char(11) not null,
 	senha varchar(99) not null,
 	email varchar(100) not null,
     telefone_celular varchar(20) not null,
-    telefone_fixo varchar(30),
-    cpnj char(18)
+    telefone_fixo varchar(30)
 )auto_increment=100;
+
+create table cadastro_pessoa_juridica(
+	id_pessoa_juridica int primary key auto_increment,
+	razao_social varchar(100) not null,
+	cpnj char(18) not null,
+	senha varchar(99) not null,
+	email varchar(100) not null,
+    telefone_fixo varchar(30)
+)auto_increment=1000;
 
 create table sensor( 
 	id_sensor int primary key auto_increment,
@@ -20,12 +28,16 @@ create table sensor(
     local_sensor varchar(30),
     status_sensor varchar(30) default 'Inativo',
     check (status_sensor = 'Ativo' or status_sensor = 'Inativo' or status_sensor = 'Manutenção'),
-	fk_usuario int,
-    foreign key (fkusuario) references cadastroUsuario(id_usuario)
+	fk_pessoa_fisica int,
+    foreign key (fk_pessoa_fisica) references cadastro_pessoa_fisica(id_pessoa_fisica),
+    fk_pessoa_juridica int,
+    foreign key (fk_pessoa_juridica) references cadastro_pessoa_juridica(id_pessoa_juridica)
 );
 
+
+
 -- Inserção de dados
-insert into cadastro_usuario values
+insert into cadastro_pessoa_fisica values
 (null,'Miguel Nunes','84726352635','12344321','miguelnunes1477@gmail.com', '1194435352'),
 (null,'Felipe Higa','48475304893','736w4e44we35','fe.higa@gmail.com', '1197456463'),
 (null,'Vitória Souza','42442960850','ff7r7fr7d','vitoria.souzasantos@hotmail.com', '1197745453'),
@@ -35,7 +47,7 @@ insert into cadastro_usuario values
 
 
 -- Inserção de dados na tabela sensor_proto -- Miguel
-insert into sensor (registro_umidade,registro_temp,registro_hora, local_sensor, status_sensor,fk_usuario) values
+insert into sensor (registro_umidade,registro_temp,registro_hora, local_sensor, status_sensor,fk_pessoa_fisica) values
 ('70%','25ºC','2020-9-15 19:16:53', 'EstufaA',101),
 ('80%','24ºC','2020-9-15 19:29:36', 'EstufaB',100),
 ('69%','24ºC','2020-9-15 18:21:23', 'EstufaC',102),
