e8cc0c0b7d847afa4bac6999050484789c0a20d6
vitoriadesouzasantos <vitoria.santos@bandtec.com.br>
2020-12-01 19:40:38 -0300

    Footer pronto, style da dashboard e começo da página perfil

--- Banco de dados/Sprint3/Sprint3.sql
+++ Banco de dados/Sprint3/Sprint3.sql
@@ -5,6 +5,7 @@ use ProjetoPI;
 create table cliente(
 idCliente int primary key auto_increment,
 tipoCliente char (2),
+check (tipoCliente = 'pf' or tipoCliente = 'pj'),
 nome_RazaoSocial varchar (100),
 cpf_cnpj varchar (14),
 telefoneFixo varchar (30),
@@ -25,53 +26,74 @@ foreign key (fkcliente) references cliente(idcliente)
 select * from Usuario;
 
 create table LocalEstufa (
-idLocalEstufa int ,
+idLocalEstufa int primary key auto_increment,
 logradouroLocal varchar (50),
 cidadeLocal varchar (40),
 bairroLocal  varchar (30),
 cepLocal varchar (15),
 complementoLocal varchar (25),
 numeroLocal varchar (10),
-fkcliente int,
-foreign key (fkcliente) references cliente(idcliente),
-primary key (idLocalEstufa, fkcliente));
+fkUsuario int,
+foreign key (fkusuario) references usuario(idusuario)
+);
 
 select * from LocalEstufa;
 
 create table Estufa (
-idEstufa int,
+idEstufa int primary key auto_increment,
 tipoCultura varchar (30),
 temperaturaMaxima decimal (3,1),
 temperaturaMinima decimal (3,1),
 umidadeMaxima int,
 umidadeMinima int,
 fklocalEstufa int,
-fkcliente int,
-foreign key (fklocalEstufa) references LocalEstufa(idLocalEstufa),
-foreign key (fkcliente) references cliente(idCliente),
-primary key (idEstufa, fklocalEstufa, fkcliente));
+foreign key (fklocalEstufa) references LocalEstufa(idLocalEstufa)
+);
 
 select * from Estufa;
 
 create table Sensor(
-idSensor int,
+idSensor int primary key auto_increment,
 statusSensor varchar (40),
+check(statusSensor = 'Ativo' or statusSensor = 'Inativo' or statusSensor = 'Em manutenção'),
 fkestufa int,
-fklocalEstufa int,
-fkcliente int,
-foreign key (fkestufa) references estufa(idEstufa),
-foreign key (fklocalEstufa) references LocalEstufa(idLocalEstufa),
-foreign key (fkcliente) references cliente(idCliente),
-primary key (idSensor, fkestufa, fklocalEstufa, fkcliente));
+foreign key (fkestufa) references estufa(idEstufa)
+);
 
 select * from Sensor;
 
 create table dadosSensor (
-idDadosSensor int,
+idDadosSensor int primary key auto_increment,
 registroUmidade decimal (3,1),
 registroTemperatura decimal (3,1),
 fksensor int,
-foreign key (fksensor) references Sensor(idSensor),
-primary key (idDadosSensor, fksensor));
+foreign key (fksensor) references Sensor(idSensor)
+);
 
 select * from dadosSensor;
+
+create table acesso(
+	fkUsuario int,
+    foreign key (fkUsuario) references usuario(idusuario),
+    fkestufa int,
+    foreign key (fkestufa) references estufa(idestufa),
+    cargo varchar(45)
+);
+select * from cliente;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
