e314cd39f17aacdea4f51c7bddf4143b7fb3a5a6
IgorSilvaZZ <igor06silva12@gmail.com>
2020-12-08 22:10:37 -0300

    Estatisticas Atualizada

--- projeto-site/public/js/estatisticas.js
+++ projeto-site/public/js/estatisticas.js
@@ -1,7 +1,7 @@
 function atualizacaoPeriodica() {
     obterdadosporsensor(30000);
     obterdadosporsensor(30002);
-    setTimeout(atualizacaoPeriodica, 5000);
+    setTimeout(atualizacaoPeriodica, 2000);
 }
 
 function obterdadosporsensor(idSensor) {
@@ -36,12 +36,6 @@ function obterdadosporsensor(idSensor) {
 
 function alertar(temperatura, umidade, idSensor) {
     // padrão para meu alerta
-    var limites = {
-        max_temperatura: 40,
-        min_temperatura: 20,
-        max_umidade: 80,
-        min_umidade: 50,
-    };
 
     var tempsTomate = {
         tempMin: 12,
@@ -60,61 +54,57 @@ function alertar(temperatura, umidade, idSensor) {
     // zerar aviso de mensagem
     var mensagem_temperatura = '';
     var mensagem_umidade = '';
-    /* var classe_temperatura = 'alerta';
-    var classe_umidade = 'alerta_umidd'; */
-
-    // comparando
-
-    /* Comparações de temperaturas de tomate */
-    if (temperatura > tempsTomate.tempMax) {
-        mensagem_temperatura = 'Temperatura do Tomate alta demais! <br>';
-        /* classe_temperatura = 'alerta alerta-alto'; */
-    }
-    if (temperatura < tempsTomate.tempMin) {
-        mensagem_temperatura = 'Temperatura do Tomate baixa demais! <br>';
-    }
-    if (umidade > tempsTomate.umiMax) {
-        mensagem_umidade += 'Umidade do Tomate alta demais! <br>';
-    }
-
-    if (umidade < tempsTomate.umiMin) {
-        mensagem_umidade = 'Umidade do Tomate baixa demais! <br>';
-    }
-
-
-    /* Comparações de temperaturas de alface */
-    if(temperatura > tempsAlface.tempMax){
-        mensagem_temperatura = 'Temperatura do Alface alta demais! <br>';
-    }
-    if(temperatura < tempsAlface.tempMin){
-        mensagem_temperatura = 'Temperatura do Alface baixa demais! <br>';
-    }
-    if(umidade > tempsAlface.umiMax){
-        mensagem_umidade = 'Umidade do Alface alta demais! <br>';
-    }
-    if(umidade < tempsAlface.umiMin){
-        mensagem_umidade = 'Umidade do Alface Baixa demais! <br>';
-    }
-    
     
     // escolhendo qual alterar
-    var div_temperatura_alterar
-    var div_umidade_alterar
 
-    if (idSensor == 30000) {
-        div_temperatura_alterar = div_alerta_temperatura
-        div_umidade_alterar = div_alerta_umidade
-    } else if (idSensor == 30002) {
-        div_temperatura_alterar = div_alerta_temperatura2
-        div_umidade_alterar = div_alerta_umidade2
+    if (idSensor == 30000) { 
+        
+        /* Estufa de Tomate */
+
+        div_alerta_temperatura.innerHTML = '';
+        div_alerta_umidade.innerHTML = '';
+
+        if (temperatura > tempsTomate.tempMax) {
+            mensagem_temperatura = 'Temperatura do Tomate alta demais! <br>';
+            div_alerta_temperatura.innerHTML = mensagem_temperatura;
+        }
+        if (temperatura < tempsTomate.tempMin) {
+            mensagem_temperatura = 'Temperatura do Tomate baixa demais! <br>';
+            div_alerta_temperatura.innerHTML = mensagem_temperatura;
+        }
+        if (umidade > tempsTomate.umiMax) {
+            mensagem_umidade = 'Umidade do Tomate alta demais! <br>';
+            div_alerta_umidade.innerHTML = mensagem_umidade;
+        }
+        if (umidade < tempsTomate.umiMin) {
+            mensagem_umidade = 'Umidade do Tomate baixa demais! <br>';
+            div_alerta_umidade.innerHTML = mensagem_umidade;
+        }
+
+    } else if (idSensor == 30002) { 
+        /* Estufa de Alface */
+
+        div_alerta_temperatura2.innerHTML = '';
+        div_alerta_umidade2.innerHTML = '';
+
+        if(temperatura > tempsAlface.tempMax){
+            mensagem_temperatura = 'Temperatura do Alface alta demais! <br>';
+            div_alerta_temperatura2.innerHTML = mensagem_temperatura
+        }
+        if(temperatura < tempsAlface.tempMin){
+            mensagem_temperatura = 'Temperatura do Alface baixa demais! <br>';
+            div_alerta_temperatura2.innerHTML = mensagem_temperatura
+        }
+        if(umidade > tempsAlface.umiMax){
+            mensagem_umidade = 'Umidade do Alface alta demais! <br>';
+            div_alerta_umidade2.innerHTML = mensagem_umidade
+        }
+        if(umidade < tempsAlface.umiMin){
+            mensagem_umidade = 'Umidade do Alface Baixa demais! <br>';
+            div_alerta_umidade2.innerHTML = mensagem_umidade
+        }
+
     } 
-
-    // alterando
-    div_temperatura_alterar.innerHTML = mensagem_temperatura;
-    /* div_temperatura_alterar.className = classe_temperatura; */
-
-    div_umidade_alterar.innerHTML = mensagem_umidade;
-    /* div_umidade_alterar.className = classe_umidade; */
 }
 
 function atualizarTela(dados, idSensor) {
@@ -148,6 +138,6 @@ function sendData() {
 
 setInterval(() => {
     sendData();
-}, 5000);
+}, 2000);
 
 window.onload = atualizacaoPeriodica;
